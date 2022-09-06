2bf79736e343808ce3617cb3263b3130b1aed883
asaduli <asadulislam.diu@gmail.com>
2022-09-06 14:59:29 +0600

    replace a character using replace method

--- String/stringOperation.js
+++ String/stringOperation.js
@@ -7,3 +7,10 @@ let str="abcdefghijklmnopqrstuvwxyz"
 
 let length=str.length;// this method calculates the length of the string
 console.log("length: " + str.length);
+
+//Replacement method for strings
+
+let newstr=str.replace('a','z');  // 
+console.log("newstr: " + newstr);
+
+
