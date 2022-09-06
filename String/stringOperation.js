368b90adea662990dd314d5d9a25f78dd6c9d576
asaduli <asadulislam.diu@gmail.com>
2022-09-06 14:04:18 +0600

    calulating string length by using String length methos

---
+++ String/stringOperation.js
@@ -0,0 +1,9 @@
+
+
+// String methods practice
+
+
+let str="abcdefghijklmnopqrstuvwxyz"
+
+let length=str.length;// this method calculates the length of the string
+console.log("length: " + str.length);
