21134d3916dcf0f33d4ed817ab7056285968407f
asaduli <asadulislam.diu@gmaiil.com>
2022-09-24 16:42:29 +0600

    parctice js escape character and re-commit

---
+++ String/escapeCharater.js
@@ -0,0 +1,43 @@
+//for practice javascript escape characters
+
+
+// \' single quote escape character
+let sigleQuoteText="Hello,Programmers \'JavaScript\' is Awsome"
+
+//   \" Double quote escape character 
+let dobleQuoteText='Hello, Allines \"Java\" is one of the best programing language'
+
+// \\ backslash
+
+let backslashText='This is \\ backslash'
+
+// \b backspace character
+
+let backspace='this is \b backspace'
+
+// console.log(backspace);
+
+// \f form feed escape character
+
+let fromFeedText='this is form feed \f text';
+
+// console.log(fromFeedText);
+
+// \n new line escape character
+
+let newline='this is line.\n thsi is another line';
+// console.log(newline);
+
+// \r carriage return
+
+let carriageReturn='this is carriage \r return';
+// console.log(carriageReturn);
+
+// \t horizantal tab
+
+let tabHorizantal='this is horizantal \t tab';
+
+let tabVirtical="this is vartical \v tab";
+
+console.log(tabHorizantal)
+console.log(tabVirtical)
