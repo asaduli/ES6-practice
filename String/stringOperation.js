1b515b9440a10bcb437905c2c73d123613d57454
asaduli <asadulislam.diu@gmaiil.com>
2022-09-24 16:49:58 +0600

    add slice method for string operation

--- String/stringOperation.js
+++ String/stringOperation.js
@@ -13,4 +13,9 @@ console.log("length: " + str.length);
 let newstr=str.replace('a','z');  // 
 console.log("newstr: " + newstr);
 
+// slice(startpostion, endposition) ..end postion not included
+let slicestr=str.slice(0,25); //slice(startpositon) [if the second parameter is not included than rest of the character will be sliced]
+
+console.log(slicestr);
+
 
